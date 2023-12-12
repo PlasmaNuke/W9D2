fe7e476dee7ca6e427249997439d1260a4a00f1e
Peter Nolan <peter.l.nolan@gmail.com>
2023-12-12 16:23:07 -0500

    finish project

--- practice-for-ch-js-browser-ttt-main/src/index.js
+++ practice-for-ch-js-browser-ttt-main/src/index.js
@@ -1,5 +1,8 @@
-Add your import statements for View and Game here
+import Game from "../ttt_node/game.js";
+import View from "./ttt-view.js";
 
 document.addEventListener("DOMContentLoaded", () => {
-  // Your code here
+  const game = new Game();
+  const figure = document.querySelector(".ttt");
+  new View(game, figure);
 });
--- practice-for-ch-js-browser-ttt-main/src/ttt-view.js
+++ practice-for-ch-js-browser-ttt-main/src/ttt-view.js
@@ -1,17 +1,55 @@
+import Game from "../ttt_node/game.js"
+
 class View {
   constructor(game, el) {
-  }
+    this.game = game;
+    this.container = el;
+    this.setupBoard();
+    document.addEventListener("click", this.handleClick.bind(this));
+    }
   
   setupBoard() {
+    const grid = document.createElement("ul");
+
+    for(let row = 0; row < 3; row++) {
+      for(let col = 0; col < 3; col++) {
+        let gridSquare = document.createElement("li");
+        gridSquare.setAttribute("data-row", row);
+        gridSquare.setAttribute("data-col", col);
+        grid.appendChild(gridSquare);
+      }
+    }
+    this.container.append(grid);
   }
   
   handleClick(e) {
+    if (e.target.nodeName === "LI") {
+      this.makeMove(e.target);
+    }
   }
 
   makeMove(square) {
+    let mark = this.game.currentPlayer;
+    let pos = [square.getAttribute("data-row"), square.getAttribute("data-col")];
+
+    if (this.game.board.isEmptyPos(pos)) {
+      this.game.playMove(pos);
+      square.classList.add("occupied")
+      if (mark === "x") {
+        square.classList.add("X")
+      } else {
+        square.classList.add("O")
+      }
+      square.innerText = mark;
+    } else {
+      alert('Invalid move');
+    }
+
+    if (this.game.board.isOver()) this.handleGameOver();
   }
   
   handleGameOver() {
+    this.container.append(`Player ${this.game.winner()} wins!`);
   }
 }
 
